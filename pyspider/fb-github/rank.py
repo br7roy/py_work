8f819f01c603cf3a2984d39ffdf019098c08db35
Tak <br7roy@gmail.com>
2019-11-04 21:43:51 +0800

    fb开源项目排名统计(后台语言)

--- pyspider/fb-github/rank.py
+++ pyspider/fb-github/rank.py
@@ -3,41 +3,45 @@ from bs4 import BeautifulSoup
 import re
 
 
-def start(page):
+def start(page, langs):
     cache = {}
-    mark = 'stargazers'
-    i = 1
-    while True:
-        try:
-            r = requests.get(page + str(i))
-            r.raise_for_status()
-            i += 1
-            soup = BeautifulSoup(r.text, 'html5lib')
-            target = soup.select("a.no-wrap.muted-link.mr-3")
 
-            for elem in target:
-                txt = str(elem)
-                group = re.search(r'href="(.*?)"', txt)
-                res = group.group(1)[1:]
-                s_index = res.rindex("/") + 1
-                if res[s_index:] != mark:
-                    continue
-                first_s_idx = res.index("/") + 1
-                p_name = res[first_s_idx:res.rindex("/")]
-                s = txt.index('</svg>')
-                e = txt.rindex('</a>')
-                star = txt[s + 7:e]
-                p_star = re.sub('(\\s+)', '', star)
-                p_star = re.sub(',', '', p_star)
-                cache.update({p_name: int(p_star)})
-        except:
-            break
+    for lang in langs:
+        mark = 'stargazers'
+        i = 1
+        while True:
+            try:
+                url = (page + str(i)).format(lang)
+                r = requests.get(url)
+                r.raise_for_status()
+                i += 1
+                soup = BeautifulSoup(r.text, 'html5lib')
+                target = soup.select("a.no-wrap.muted-link.mr-3")
+
+                for elem in target:
+                    txt = str(elem)
+                    group = re.search(r'href="(.*?)"', txt)
+                    res = group.group(1)[1:]
+                    s_index = res.rindex("/") + 1
+                    if res[s_index:] != mark:
+                        continue
+                    first_s_idx = res.index("/") + 1
+                    p_name = res[first_s_idx:res.rindex("/")]
+                    s = txt.index('</svg>')
+                    e = txt.rindex('</a>')
+                    star = txt[s + 7:e]
+                    p_star = re.sub('(\\s+)', '', star)
+                    p_star = re.sub(',', '', p_star)
+                    cache.update({p_name: int(p_star)})
+            except:
+                break
     return cache
 
 
 if __name__ == '__main__':
-    s_page = r'https://github.com/facebook?page='
-    cache = start(s_page)
+    s_page = r'https://github.com/facebook?language={}&page='
+    lang = ["c%2B%2B","java","python"]
+    cache = start(s_page, lang)
     print('total project ', len(cache))
     cache = sorted(cache.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
     top10 = cache[:10]
