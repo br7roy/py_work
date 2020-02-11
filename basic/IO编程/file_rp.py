878bedd93f49d5004df9d03849fce8fa5500da5b
Tak <br7roy@gmail.com>
2020-02-11 23:09:02 +0800

    fix: 修改某个目录下所有的文件，按照字符匹配的方式修改目录中所有的文件（包括子文件夹中的文件）

--- basic/IO编程/file_rp.py
+++ basic/IO编程/file_rp.py
@@ -1,10 +1,10 @@
-# 修改文件中的字符串
+# 修改文件目录下所有文件，将文件中内容匹配的字符串进行替换
 import sys
 import os
 
 
 def run():
-    path = r'E:\Developer\project\pronetway\combat-platform\src'
+    path = r'E:\Developer\project\github\experiment\src\main\java'
     # path = r'E:\hello'
     for root, dirs, files in os.walk(path):
         for file in files:
@@ -12,8 +12,8 @@ def run():
             fp = os.path.join(root, file)
             with open(fp, 'r', encoding='utf-8') as f:
                 for line in f:
-                    if 'Rust' in line:
-                        line = line.replace('Rust', 'Tak')
+                    if 'rust' in line:
+                        line = line.replace('rust', 'tak')
                     data += line
             with open(fp, 'w', encoding='utf-8')as f:
                 f.write(data)
