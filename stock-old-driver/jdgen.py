cd98b2a82eb6fa6c70137fd16f0222005943dd70
Tak <br7roy@gmail.com>
2020-06-18 10:05:27 +0800

    修改文件内容

--- stock-old-driver/jdgen.py
+++ stock-old-driver/jdgen.py
@@ -2,20 +2,26 @@ import os
 
 
 def run():
-    path = r'D:\project\proneteway\ideaprj\video-security\video-security'
+    path = r'D:\project\github\hadoop-ecosystem'
 
-    kw = r'Rust'
+    kw = r'com.rust'
     try:
         for root, dirs, files in os.walk(path):
+            if ".git "  in root: continue
             for file in files:
-                if file.endswith(".jar") or file.endswith(".class") or file.endswith(".jar.original"):
+                if file.endswith(".jar") or file.endswith(".class") or file.endswith(".jar.original") \
+                        or file.endswith(".git"):
+                    continue
+                if not file.endswith(".java") and not file.endswith(".scala") \
+                        and not file.endswith("pom.xml") \
+                        and not file.endswith(".jsp"):
                     continue
                 data = ""
                 fp = os.path.join(root, file)
                 with open(fp, 'r', encoding='utf-8') as f:
                     for line in f:
                         if kw in line:
-                            line = line.replace(kw, 'Tak')
+                            line = line.replace(kw, r'tk.tak')
                         data += line
                 with open(fp, 'w', encoding='utf-8')as f:
                     f.write(data)
